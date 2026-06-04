83bf32ad75d6a5542314ae2db9bf784758a57a02
purvapatil11 <purvapatil.dev@gmail.com>
2026-06-04 10:50:53 +0530

    feat: add meeting controls for audio, video and room exit

--- frontend/src/components/meeting/ControlsBar.tsx
+++ frontend/src/components/meeting/ControlsBar.tsx
@@ -2,14 +2,33 @@
 // Camera
 // Share 
 // Leave
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useLocalParticipant, useRoomContext } from "@livekit/components-react";
 export default function ControlsBar() {
+    const room = useRoomContext();
+    const router = useRouter();
+    const { localParticipant } = useLocalParticipant()
+
+    const leaveMeeting = async() => {
+        await room.disconnect();
+        router.push("/join");
+    }
+    const toggleMic = async() => {
+        const enabled = localParticipant.isMicrophoneEnabled;
+
+        await localParticipant.setMicrophoneEnabled(!enabled);
+    }
+    const toggleCamera = async() => {
+        const enabled = localParticipant.isCameraEnabled;
+        await localParticipant.setCameraEnabled(!enabled);
+    }
     return (
         <div>
-            <h1>Controls Bar</h1>
-            <h1>Mute</h1>
-            <h1>Camera</h1>
-            <h1>Share</h1>
-            <h1>Leave</h1>
+        <button onClick={leaveMeeting}>Leave Meeting</button>
+        <button onClick={toggleMic}>Mic</button>
+        <button onClick={toggleCamera}>Camera</button>
         </div>
     )
 }
